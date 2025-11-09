4806dad3dac390e57304bbc8ae5399a3fee92bf1
vpindore <vikas_361@hotmail.com>
2025-11-09 15:24:54 +0530

    Assignment 1

---
+++ Assignment1/BasicMathOperation.py
@@ -0,0 +1,7 @@
+a = float(input('Ener the First Number'));
+b = float(input('Enter the second Number'));
+
+print('Addition', a+b)
+print('Substraction', a-b)
+print('Multiplication', a*b)
+print('Division', a/b)
---
+++ Assignment1/PersonalizedGreeting.py
@@ -0,0 +1,4 @@
+firstName = input('Enter the First Name')
+lastName = input('Enter the Last Name')
+
+print("Hello,", firstName, lastName + "!", "Welcome to  the Python Program")
